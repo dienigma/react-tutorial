944e0d2440cce78f77e19708831b461727a3c35e
dienigma <joshi.chinmay09@gmail.com>
2019-04-19 22:32:00 +0530

    Built a form to Add rpeople

--- src/App.js
+++ src/App.js
@@ -1,38 +1,33 @@
 import React, { Component } from "react";
 import Table from "./Table";
-
+import Form from "./Form";
 class App extends Component {
-  render() {
-    const characters = [
-      {
-        name: "Chinmay",
-        job: "Project Manager"
-      },
-      {
-        name: "Adham",
-        job: "Project Manager"
-      },
-      {
-        name: "Afzal",
-        job: "Mentor"
-      },
-      {
-        name: "Mayank",
-        job: "Full-Stack Devloper"
-      },
-      {
-        name: "Nakul",
-        job: "Full-Stack Developer"
-      },
-      {
-        name: "Shahid",
-        job: "Front-end Developer"
-      }
-    ];
+  state = {
+    characters: []
+  };
 
+  removeCharacter = index => {
+    const { characters } = this.state;
+
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index;
+      })
+    });
+  };
+
+  handleSubmit = character => {
+    this.setState({ characters: [...this.state.characters, character] });
+  };
+
+  render() {
     return (
       <div className="container">
-        <Table characterData={characters} />
+        <Table
+          characterData={this.state.characters}
+          removeCharacter={this.removeCharacter}
+        />
+        <Form handleSubmit={this.handleSubmit} />
       </div>
     );
   }
