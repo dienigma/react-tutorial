fff7cbdd72aeb3be091e16b0b02939499dd2e9f3
dienigma <joshi.chinmay09@gmail.com>
2019-04-19 22:50:31 +0530

    Updates

---
+++ precache-manifest.aa7c70accc286d31cb27323b95e0e468.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1083ba0eae9645672c05",
+    "url": "/react-tutorial/static/css/main.369536da.chunk.css"
+  },
+  {
+    "revision": "1083ba0eae9645672c05",
+    "url": "/react-tutorial/static/js/main.25cf7390.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/react-tutorial/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "45d6a9281f161987defc64b3d0ed150c",
+    "url": "/react-tutorial/index.html"
+  }
+];
