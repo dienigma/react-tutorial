3226c3657b9d655f272ef27c1f951c88a0ae9acd
dienigma <joshi.chinmay09@gmail.com>
2019-04-19 21:11:46 +0530

    Passing Down props

--- src/App.js
+++ src/App.js
@@ -3,9 +3,36 @@ import Table from "./Table";
 
 class App extends Component {
   render() {
+    const characters = [
+      {
+        name: "Chinmay",
+        job: "Project Manager"
+      },
+      {
+        name: "Adham",
+        job: "Project Manager"
+      },
+      {
+        name: "Afzal",
+        job: "Mentor"
+      },
+      {
+        name: "Mayank",
+        job: "Full-Stack Devloper"
+      },
+      {
+        name: "Nakul",
+        job: "Full-Stack Developer"
+      },
+      {
+        name: "Shahid",
+        job: "Front-end Developer"
+      }
+    ];
+
     return (
       <div className="container">
-        <Table />
+        <Table characterData={characters} />
       </div>
     );
   }
